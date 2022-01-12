1d502d92db21b15ccf180db6eaab182bba1bd669
Karen Okonkwo <karenokonkwo29@gmail.com>
2022-01-13 00:59:48 +0100

    Include solution for roman to integer

---
+++ Easy/13. Roman to Integer.py
@@ -0,0 +1,85 @@
+"""
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+    Given a roman numeral, convert it to an integer.
+
+
+
+    Example 1:
+
+    Input: s = "III"
+    Output: 3
+    Explanation: III = 3.
+    Example 2:
+
+    Input: s = "LVIII"
+    Output: 58
+    Explanation: L = 50, V= 5, III = 3.
+    Example 3:
+
+    Input: s = "MCMXCIV"
+    Output: 1994
+    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+    Constraints:
+
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+"""
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        special_symbols = {
+            "I": {"V": 4, "X": 9},          
+            "X": {"L": 40, "C": 90},          
+            "C": {"D": 400, "M": 900}
+        }
+
+        pointer = 0
+        result = 0
+
+        while pointer < len(s):
+            current_char = s[pointer]
+
+            if special_symbols.get(current_char):
+                if pointer + 1 < len(s):
+                    next_char = s[pointer + 1]
+                else:
+                    next_char = None
+
+                if special_symbols[current_char].get(next_char):
+                    result = result + special_symbols[current_char][next_char]
+                    pointer = pointer + 2
+                    continue
+            
+            # if it gets to this point, handle as a single character
+            result = result + symbols[current_char]
+            pointer = pointer + 1
+
+        return result
